142cb23e19e6d5f14186de608e1caea0a16c848d
Student <student@techs-air.home>
2021-02-15 21:11:29 -0500

    Week 6

--- 06-07-Database_Exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
+++ 06-07-Database_Exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
@@ -1,11 +1,17 @@
 package com.techelevator.projects.model.jdbc;
 
+import java.time.LocalDate;
+
+import java.util.Date
+import java.sql.Date
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.EmployeeDAO;
@@ -13,6 +19,7 @@ import com.techelevator.projects.model.EmployeeDAO;
 public class JDBCEmployeeDAO implements EmployeeDAO {
 
 	private JdbcTemplate jdbcTemplate;
+	
 
 	public JDBCEmployeeDAO(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -20,16 +27,42 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	
 	@Override
 	public List<Employee> getAllEmployees() {
-		return new ArrayList<>();
+		String sql = "SELECT first_name, last_name FROM employee WHERE first_name NOT NULL OR last_name NOT NULL";
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(sql);
+		
+		List<Employee>EmployeeList = new ArrayList<Employee>();
+		while (rows.next()) {
+			EmployeeList.add(mapRowToEmployee(rows));
+		}
+		
+		return EmployeeList;
 	}
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		return new ArrayList<>();
+		String sql = "SELECT first_name, last_name FROM employee WHERE first_name like %" + "?" + 
+				"% AND last_name like %" + "?" + "%";
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(sql);
+		List<Employee> EmployeeList = new ArrayList<Employee>();
+		
+		while(rows.next()) {
+			EmployeeList.add(mapRowToEmployee(rows));
+		}
+		
+		return EmployeeList;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByDepartmentId(long id) {
+		String sql = "SELECT first_name, last_name FROM employee WHERE department_id = ?";
+		SqlRowSet rows = jdbcTemplate.queryForRowSet(sql, id);
+		
+		List<Employee> EmployeeByDepartment = new ArrayList<Employee>();
+		
+		while(rows.next()) {
+			EmployeeByDepartment.add(rows)
+		}
+		
 		return new ArrayList<>();
 	}
 
@@ -46,6 +79,23 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	@Override
 	public void changeEmployeeDepartment(Long employeeId, Long departmentId) {
 		
+	}
+	
+	
+	
+	private Employee mapRowToEmployee(SqlRowSet rows) {
+		Employee employee = new Employee();
+		
+		// String gender = employee.setGender("gender");
+		
+		employee.setId(rows.getLong("employee_id"));
+		employee.setFirstName(rows.getString("first_name") );
+		employee.setLastName(rows.getString("last_name"));
+		employee.setBirthDay(rows.getDate("birthDay"));
+		employee.setGender(rows.getGender("gender"));
+		employee.setHireDate(rows.getDate("hire_date"));
+		
+		return employee;
 	}
 
 }
